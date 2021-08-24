ce70e10bbf5faa9329f7ca2e8b230b5c4eadff3b
Marcus Veloso <desenvolvedoroficial@gmail.com>
2021-08-23 22:58:54 -0300

    add atutenticação

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :acessorios
   resources :calcados
   resources :roupas
+  
   root to: "roupas#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
