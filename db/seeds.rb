8d8bc218fbef66860dbc67c65f5f0fcd5449b663
Marcus Veloso <desenvolvedoroficial@gmail.com>
2021-08-26 00:25:47 -0300

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,96 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p 'Inserindo Roupas...'
+
+Roupa.create([
+  {tipo_roupa: 'Camisa social', descricao:'', tamanho: 'M', marca:'Sanremo', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa social', descricao:'', tamanho: 'M', marca:'Sanremo', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa social', descricao:'', tamanho: 'M', marca:'Sanremo', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa social', descricao:'', tamanho: 'M', marca:'Sanremo', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa social', descricao:'', tamanho: 'M', marca:'Sanremo', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa social', descricao:'', tamanho: 'M', marca:'Sanremo', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa social', descricao:'', tamanho: 'M', marca:'Sanremo', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa social', descricao:'', tamanho: 'M', marca:'Sanremo', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa social', descricao:'', tamanho: 'M', marca:'Sanremo', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa social', descricao:'', tamanho: 'M', marca:'Sanremo', favorito:false, ativo:true},
+  
+  {tipo_roupa: 'Camiseta Xadrez', descricao:'Verde com azul', tamanho: 'M', marca:'', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta Xadrez', descricao:'Cinza com rosa', tamanho: 'M', marca:'', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta Xadrez', descricao:'Verde com preto', tamanho: 'M', marca:'', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta Xadrez', descricao:'Azul com branco', tamanho: 'M', marca:'', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta Xadrez', descricao:'Florida', tamanho: 'M', marca:'', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta Xadrez', descricao:'Preta', tamanho: 'M', marca:'', favorito:false, ativo:true},
+
+  {tipo_roupa: 'Camiseta esportiva', descricao:'Vancouver', tamanho: 'M', marca:'', favorito:true, ativo:true},
+  {tipo_roupa: 'Camisa Manga Longa', descricao:'Kawasaki', tamanho: 'G', marca:'', favorito:true, ativo:true},
+  
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta esportiva', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  
+  {tipo_roupa: 'Short', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Short', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Short', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  
+  {tipo_roupa: 'Short', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Short', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Short', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Short', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Short', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  
+  {tipo_roupa: 'Calça', descricao:'', tamanho: '40', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Calça', descricao:'', tamanho: '40', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Calça', descricao:'', tamanho: '40', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Calça', descricao:'', tamanho: '40', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Calça', descricao:'', tamanho: '40', marca:'S/M', favorito:false, ativo:true},
+
+  {tipo_roupa: 'Camisa de evento', descricao:'', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa de evento', descricao:'Hackathon 2018', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa de evento', descricao:'Hackathon 2019', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa de evento', descricao:'SW 2019', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa de evento', descricao:'Mentor Hackathon 2019', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa de evento', descricao:'GovTech 2020', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camisa de evento', descricao:'Case 2019', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  
+  {tipo_roupa: 'Camiseta Jogo', descricao:'Gears of War', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta Jogo', descricao:'Gears of War', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  
+  {tipo_roupa: 'Camiseta', descricao:'Banda K-Torze', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta', descricao:'Banda K-Torze', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+  {tipo_roupa: 'Camiseta', descricao:'Banda K-Torze', tamanho: 'M', marca:'S/M', favorito:false, ativo:true},
+])
+
+p 'Roupas inseridas com sucesso!'
+p ''
+p 'Inserindo calçados...'
+
+Calcado.create([
+  {tipo_calcado: 'Tênis', marca:'Asics', tamanho:'41', favorito:false, ativo:true},
+  {tipo_calcado: 'Tênis', marca:'Everlast', tamanho:'41', favorito:false, ativo:true},
+  {tipo_calcado: 'Tênis', marca:'Puma', tamanho:'41', favorito:false, ativo:true},
+  {tipo_calcado: 'Tênis', marca:'Ferricelli', tamanho:'39', favorito:false, ativo:true},
+  {tipo_calcado: 'Cotunto', marca:'All Star', tamanho:'39', favorito:false, ativo:true}
+])
+
+p 'Calçados inseridos com sucesso!'
+p ''
+p 'Inserindo Acessórios...'
+
+Acessorio.create([
+  {tipo_acessorio:'Cordão', descricao: 'One Piece', marca:'S/M', tamanho:'', favorito:false, ativo:true},
+  {tipo_acessorio:'Cordão', descricao: 'Minecraft', marca:'S/M', tamanho:'', favorito:false, ativo:true},
+  {tipo_acessorio:'boné', descricao: 'Cinza', marca:'Vancouver', tamanho:'Único', favorito:false, ativo:true},
+])
+
+p 'Acessórios inseridos com sucesso!'
